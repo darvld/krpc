ab29464ce4cce4da6f027d8322e25b12e82fea07
darvld <dvaldespino00@gmail.com>
2021-07-04 19:27:34 -0400

    [compiler] Require interfaces or abstract classes for service definitions.

--- krpc-compiler/src/main/kotlin/com/github/darvld/krpc/compiler/ServiceVisitor.kt
+++ krpc-compiler/src/main/kotlin/com/github/darvld/krpc/compiler/ServiceVisitor.kt
@@ -3,19 +3,28 @@ package com.github.darvld.krpc.compiler
 import com.github.darvld.krpc.Service
 import com.github.darvld.krpc.compiler.model.ServiceDefinition
 import com.google.devtools.ksp.getDeclaredFunctions
+import com.google.devtools.ksp.symbol.ClassKind.INTERFACE
 import com.google.devtools.ksp.symbol.KSClassDeclaration
 import com.google.devtools.ksp.symbol.KSNode
+import com.google.devtools.ksp.symbol.Modifier
 import com.google.devtools.ksp.validate
 import com.google.devtools.ksp.visitor.KSDefaultVisitor
 
 class ServiceVisitor : KSDefaultVisitor<Unit, ServiceDefinition>() {
     private val methodVisitor = ServiceMethodVisitor()
 
+    private fun reportError(inClass: KSClassDeclaration, message: String) {
+        throw IllegalStateException("Error while processing service definition ${inClass.qualifiedName?.asString()}: $message")
+    }
+
     override fun defaultHandler(node: KSNode, data: Unit): ServiceDefinition {
         throw IllegalStateException("Service visitor can only visit service definition interfaces")
     }
 
     override fun visitClassDeclaration(classDeclaration: KSClassDeclaration, data: Unit): ServiceDefinition {
+        if (classDeclaration.classKind != INTERFACE && Modifier.ABSTRACT !in classDeclaration.modifiers)
+            reportError(classDeclaration, "Service definitions must be interfaces or abstract classes")
+
         // The annotation arguments contain the names for the service, the provider and the client (if specified)
         val annotationArgs = classDeclaration.annotations.find {
             it.shortName.getShortName() == Service::class.simpleName
